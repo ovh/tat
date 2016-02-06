981803f1c8d40777f38d05fc2ca325677a9734d0
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-02-06 22:09:07 +0100

    test: add without nsec
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

--- utils/tat_date_test.go
+++ utils/tat_date_test.go
@@ -11,3 +11,9 @@ func TestSplitFloatForTimeUnix(t *testing.T) {
 	assert.True(t, a == 12345678, "a should be equals to 12345678")
 	assert.True(t, b == 887766000, "b should be equals to 887766000")
 }
+
+func TestSplitFloatForTimeUnixSec(t *testing.T) {
+	a, b := SplitFloatForTimeUnix(12345678)
+	assert.True(t, a == 12345678, "a should be equals to 12345678")
+	assert.True(t, b == 0, "b should be equals to 0")
+}
