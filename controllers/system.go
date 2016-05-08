452c718c1051e1df11ff95725397ee719bd61653
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-05-08 18:30:34 +0200

    [auto] bump version to v1.2.0

--- controllers/system.go
+++ controllers/system.go
@@ -8,7 +8,7 @@ import (
 )
 
 // VERSION is version of Tat.
-const VERSION = "1.1.0"
+const VERSION = "1.2.0"
 
 // SystemController contains all methods about version
 type SystemController struct{}
