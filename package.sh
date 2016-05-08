f4a56bb6e8da069c0c93c56b6802a85c9f1d7294
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-05-08 18:18:26 +0200

    chore: go 1.6.2 and add logs on packaging
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

--- package.sh
+++ package.sh
@@ -7,4 +7,7 @@ for GOOS in darwin linux ; do
     export GOOS=$GOOS
     export GOARCH=$GOARCH
     CGO_ENABLED=0 go build -a -installsuffix cgo -o bin/tat-${architecture}
+    echo "file bin/tat-${architecture}"
+    file bin/tat-${architecture}
+    echo "ldd bin/tat-${architecture}"
 done
