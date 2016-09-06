d127051b11edd469f3d347e33557f9e435b79b91
François Samin <francois.samin@corp.ovh.com>
2016-09-06 14:29:48 +0200

    tests: TestListTopicsFromCache

--- api/topics_test.go
+++ api/topics_test.go
@@ -43,3 +43,46 @@ func TestTopicCreateListAndDelete(t *testing.T) {
 	}
 
 }
+
+func TestListTopicsFromCache(t *testing.T) {
+	tests.Init(t)
+	tests.Router(t)
+
+	tests.Handle(t, http.MethodPost, "/topic", tests.FakeAuthHandler(t, tests.AdminUser, "TAT-TEST", true, false), topicsController.Create)
+	tests.Handle(t, http.MethodPut, "/topic/add/admingroup", tests.FakeAuthHandler(t, tests.AdminUser, "TAT-TEST", true, false), topicsController.AddAdminGroup)
+	tests.Handle(t, http.MethodGet, "/topics", tests.FakeAuthHandler(t, tests.AdminUser, "TAT-TEST", true, false), topicsController.List)
+	tests.Handle(t, http.MethodDelete, "/topic/*topic", tests.FakeAuthHandler(t, tests.AdminUser, "TAT-TEST", true, false), topicsController.Delete)
+
+	client := tests.TATClient(t, tests.AdminUser)
+	topic, err := client.TopicCreate(tat.TopicCreateJSON{
+		Topic:       "/" + tests.RandomString(t, 10),
+		Description: "this is a test",
+	})
+
+	assert.NotNil(t, topic)
+	assert.NoError(t, err)
+	if topic == nil {
+		t.Fail()
+		return
+	}
+	t.Logf("Topic %s created", topic.Topic)
+
+	topics, err := client.TopicList(nil)
+	assert.NotNil(t, topics)
+	assert.NoError(t, err)
+
+	err = client.TopicAddAdminGroup(topic.Topic, "admin_group", false)
+	assert.NoError(t, err)
+
+	topics, err = client.TopicList(nil)
+	var found bool
+	for _, to := range topics.Topics {
+		if to.ID == topic.ID {
+			assert.NotEmpty(t, to.AdminGroups)
+		}
+	}
+	assert.True(t, found)
+
+	err = client.TopicDelete(topic.Topic)
+	assert.NoError(t, err)
+}
