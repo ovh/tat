441a89c8994093c7a76cd1d5feb3c1ec688f9c18
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-01-21 23:48:18 +0100

    feat: --stream --exec --onlyMsgCount
    --stream --exec 'myLights --pulse blue --duration=1000'
    
    With only --onlyMsgCount=true :
    
    --exec min:max:cmda --exec min:max:cmdb, example: --exec 0:4:'cmdA' --exec 5::'cmdb'

--- tatcli/message/list.go
+++ tatcli/message/list.go
@@ -3,6 +3,7 @@ package message
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
@@ -14,9 +15,9 @@ import (
 )
 
 var (
-	criteria tat.MessageCriteria
-	stream   bool
-	execMsg  string
+	criteria         tat.MessageCriteria
+	stream           bool
+	execMsg, execErr []string
 )
 
 func init() {
@@ -65,7 +66,8 @@ func init() {
 	cmdMessageList.Flags().StringVarP(&criteria.OnlyCount, "onlyCount", "", "", "--onlyCount=true: only count messages, without retrieve msg. limit, skip, treeview criterias are ignored.")
 	cmdMessageList.Flags().StringVarP(&criteria.SortBy, "sortBy", "", "", "--sortBy=-dateCreation: sort message. Use '-' to reverse sort. Default is --sortBy=-dateCreation. You can use: text, topic, inReplyOfID, inReplyOfIDRoot, nbLikes, labels, likers, votersUP, votersDown, nbVotesUP, nbVotesDown, userMentions, urls, tags, dateCreation, dateUpdate, author, nbReplies")
 	cmdMessageList.Flags().BoolVarP(&stream, "stream", "s", false, "stream messages --stream. Request tat each 10s, default sort: dateUpdate")
-	cmdMessageList.Flags().StringVarP(&execMsg, "exec", "", "", "--stream required. exec a cmd on each new message: --stream --exec 'myLights --pulse blue --duration=1000'")
+	cmdMessageList.Flags().StringSliceVarP(&execMsg, "exec", "", nil, `--stream required. Exec a cmd on each new message: --stream --exec 'myLights --pulse blue --duration=1000' With only --onlyMsgCount=true : --exec min:max:cmda --exec min:max:cmdb, example: --exec 0:4:'cmdA' --exec 5::'cmdb'`)
+	cmdMessageList.Flags().StringSliceVarP(&execErr, "execErr", "", nil, `--stream required. Exec a cmd on each error while requesting tat: --stream --exec 'myLights --pulse blue --duration=1000' --execErr 'myLights --pulse red --duration=2000'`)
 }
 
 var cmdMessageList = &cobra.Command{
@@ -100,15 +102,30 @@ func cmdMessageListStream(topic string) {
 	c := internal.Client()
 	lastTime := float64(time.Now().Unix())
 	lastID := ""
+	lastCount := -1
 
 	for {
 		criteria.Skip = 0
 		criteria.Limit = 10
 		criteria.SortBy = "dateUpdate"
 		criteria.DateMinUpdate = fmt.Sprintf("%f", lastTime)
+
+		if criteria.OnlyCount == tat.True {
+			out, err := c.MessageCount(topic, &criteria)
+			if err != nil {
+				processExecError(err)
+				continue
+			}
+			processCount(out.Count)
+			if out.Count != lastCount {
+				time.Sleep(10 * time.Second)
+			}
+			continue
+		}
+
 		out, err := c.MessageList(topic, &criteria)
 		if err != nil {
-			fmt.Printf("Error:%s", err)
+			processExecError(err)
 			continue
 		}
 
@@ -126,10 +143,55 @@ func cmdMessageListStream(topic string) {
 	}
 }
 
+func processExecError(err error) {
+	fmt.Printf("Error:%s", err)
+	for _, ex := range execErr {
+		execCmd(ex)
+	}
+}
+
+func processCount(count int) {
+	fmt.Printf("count:%d\n", count)
+	h := "Invalid --exec with --onlyMsgCount. Use --exec min:max:cmd, example: --exec 0:4:'myLights --pulse blue --duration=1000'\n"
+
+	maxInt := int(^uint(0) >> 1)
+
+	for _, ex := range execMsg {
+		tuple := strings.Split(ex, ":")
+		if len(tuple) != 3 {
+			internal.Exit(h)
+		}
+
+		min, errmin := strconv.Atoi(tuple[0])
+		if errmin != nil {
+			internal.Exit(h)
+		}
+
+		max := maxInt
+		if tuple[1] != "" {
+			var errmax error
+			max, errmax = strconv.Atoi(tuple[1])
+			if errmax != nil {
+				internal.Exit(h)
+			}
+		}
+
+		if count >= min && count <= max {
+			execCmd(tuple[2])
+		}
+	}
+}
+
 func processMsg(msg tat.Message) {
 	fmt.Printf("%s %s %s\n", time.Unix(int64(msg.DateUpdate), 0).Format(time.Stamp), msg.Author.Username, msg.Text)
-	opts := strings.Split(execMsg, " ")
-	if execMsg != "" {
+	for _, ex := range execMsg {
+		execCmd(ex)
+	}
+}
+
+func execCmd(ex string) {
+	opts := strings.Split(ex, " ")
+	if ex != "" {
 		s := spinner.New(spinner.CharSets[35], 100*time.Millisecond)
 		cmd := exec.Command(opts[0], opts[1:]...)
 		s.Start()
@@ -142,4 +204,5 @@ func processMsg(msg tat.Message) {
 		}
 		s.Stop()
 	}
+
 }
