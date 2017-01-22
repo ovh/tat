4e6ff3229e73b4d76a92d8f34040077bc1ad5419
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-01-22 14:15:27 +0100

    [auto] bump version to v5.2.1

--- types.go
+++ types.go
@@ -3,7 +3,7 @@ package tat
 // Version of Tat
 // One Line for this, used by release.sh script
 // Keep "const Version on one line"
-const Version = "5.2.0"
+const Version = "5.2.1"
 
 const (
 	// TatHeaderUsername is Tat_username header
