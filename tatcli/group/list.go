e77a4893e097b92ef9b1649d3db39e379af06692
Maxime Caruchet <maxime.caruchet@corp.ovh.com>
2018-02-20 14:50:27 +0100

    feat: Support search with criteria on groups in TaT CLI

--- tatcli/group/list.go
+++ tatcli/group/list.go
@@ -1,16 +1,29 @@
 package group
 
 import (
+	"github.com/ovh/tat"
 	"github.com/ovh/tat/tatcli/internal"
 	"github.com/spf13/cobra"
 )
 
+var (
+	criteria tat.GroupCriteria
+)
+
+func init() {
+	cmdGroupList.Flags().StringVarP(&criteria.IDGroup, "idGroup", "", "", "Search by id of group")
+	cmdGroupList.Flags().StringVarP(&criteria.Name, "name", "", "", "Search by group name (supports regex syntax), example: 'foo' will search for the group exactly named 'foo' while '.*foo.*' will search for groups containing 'foo' in their name")
+	cmdGroupList.Flags().StringVarP(&criteria.Description, "description", "", "", "Search by description of group")
+	cmdGroupList.Flags().StringVarP(&criteria.DateMinCreation, "dateMinCreation", "", "", "Filter result on dateCreation, timestamp Unix Format")
+	cmdGroupList.Flags().StringVarP(&criteria.DateMaxCreation, "dateMaxCreation", "", "", "Filter result on dateCreation, timestamp Unix format")
+}
+
 var cmdGroupList = &cobra.Command{
 	Use:   "list",
-	Short: "List all groups: tatcli group list <skip> <limit>",
+	Short: "List all groups: tatcli group list [<skip>] [<limit>], tatcli group list -h to see all criteria",
 	Run: func(cmd *cobra.Command, args []string) {
-		skip, limit := internal.GetSkipLimit(args)
-		out, err := internal.Client().GroupList(skip, limit)
+		criteria.Skip, criteria.Limit = internal.GetSkipLimit(args)
+		out, err := internal.Client().GroupList(&criteria)
 		internal.Check(err)
 		internal.Print(out)
 	},
